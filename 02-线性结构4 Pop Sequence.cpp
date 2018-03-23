dc8bccc01dad165339e6bf4e2ff95d3aaa7ed644
Yiming Li <34473798+tracker097@users.noreply.github.com>
2018-03-23 12:45:59 +0800

    Update 02-线性结构4 Pop Sequence.cpp

--- 02-线性结构4 Pop Sequence.cpp
+++ 02-线性结构4 Pop Sequence.cpp
@@ -1,46 +1,88 @@
 #include <iostream>
-#include <stack>
+#include <malloc.h>
 using namespace std;
+
+typedef int ElemType;
+typedef int Position;
+const ElemType ERROR = -1;
+
+struct SNode{
+	ElemType * Data;
+	Position top;
+	int MaxSize;
+};
+typedef struct SNode * Stack;
+
+Stack Create( int MaxSize )
+{
+	Stack s = (Stack)malloc(sizeof(struct SNode));
+	s -> Data = (ElemType *)malloc(MaxSize * sizeof(ElemType));
+	s -> top = -1;
+	s -> MaxSize = MaxSize;
+	return s;
+}
+
+bool isFull( Stack s )
+{
+	return ( s -> MaxSize - 1 == s -> top );
+	return false;
+}
+
+bool isEmpty( Stack s )
+{
+	return ( -1 == s -> top );
+}
+
+bool push( Stack s, ElemType e )
+{
+	if ( isFull(s) )	return false;
+	s -> Data[++(s -> top)] = e;
+	return true;
+}
+
+ElemType pop( Stack s )
+{
+	if ( isEmpty(s) )	return ERROR;
+	return s -> Data[(s -> top)--];		
+}
 
 int main()
 {
-//	freopen("F://input.txt","r",stdin);
+	freopen("F://input.txt","r",stdin);
 	int M,N,K;
 	cin >> M >> N >> K;
-	while ( K-- ) {
-		bool flag = true;
-		int ptr = 1;
-		stack <int> s;
-		int length = 0;
-		int inp[N + 1];
-		for ( int i = 1; i <= N; i++ )	cin >> inp[i];
-		int i = 1;
-		while ( ptr <= N ) {
-			bool do_flag = false;
-			if ( inp[ptr] == i ) {
-//				do_flag = true;
-				ptr++;
+	while (K) {
+		Stack s;
+		s = Create(M);
+		int ptr;
+		cin >> ptr;
+		int check = N;
+		int start = 1;
+		s -> Data[s -> top] = -1;
+		while (check) {
+//			栈顶元素和当前输入的元素相等 
+			if ( s -> Data[s -> top] == ptr ) {
+				ElemType temp = pop(s);
+//				start控制下一次批量放入堆栈的起点 
+				start = (ptr >= start? ptr + 1 : start);
+				check--;
+//				如果一整行都check完毕了，那么就防止把下一行的输入在这一行 
+				if ( check )	cin >> ptr;
 			}
-			else if ( inp[ptr] == s.top() ) {
-				s.pop();
-				length--;
-				ptr++;
-//				s.push(i);
-				do_flag = true;
+//			栈顶元素和当前元素不等 
+			else if ( s -> Data[s -> top] != ptr ) {
+//			从check完毕的起始点开始到当前值全部入栈 
+				for ( int i = start; i <= ptr; i++ )	push(s,i);
 			}
-			else if ( length >= M ) {
-				flag = false;
-				break;
-			}
-			else {
-				s.push(i);
-				length++;
-			}
-			if ( !do_flag && i < N && length < M )
-//			if ( i < N && length < M )
-				i++;
+//			一种情况是栈满了但是当前值并不等于栈顶元素，即再也放不进去了 
+			if ( isFull(s) && ptr != s -> Data[s -> top] )	break;
+//			第二种情况是栈顶元素比当前值还要大，按照逻辑当前值必定先于其输出，不合逻辑 
+			if ( ptr < s -> Data[s -> top] )	break;
 		}
-		cout << (flag? "YES" : "NO") << endl; 
+//		注意不要忘了把当前行所有值读入完毕 
+		for ( int i = 1; i < check; i++ )	cin >> start;
+		cout << (check? "NO" : "YES") << endl;
+		K--; 
 	}
 	return 0;
 }
